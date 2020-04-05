4a1647269e811fa29b112e958e52d05eedecae1c
jeffreymzd <mzhde@hotmail.com>
2020-04-05 00:04:03 -0400

    Add initial project

---
+++ src/main/java/com/github/jeffrey/spring/boot/sfgmsscbrewerygateway/SfgMsscBreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.github.jeffrey.spring.boot.sfgmsscbrewerygateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgMsscBreweryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgMsscBreweryGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jeffrey/spring/boot/sfgmsscbrewerygateway/SfgMsscBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jeffrey.spring.boot.sfgmsscbrewerygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMsscBreweryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
