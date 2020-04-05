baa18be5fb6d81eea426c3b0e795a0578a6e2ee5
jeffreymzd <mzhde@hotmail.com>
2020-04-05 15:06:18 -0400

    Add routes for beer inventory services

--- src/main/java/com/github/jeffrey/spring/boot/sfgmsscbrewerygateway/config/LocalHostRouteConfiguration.java
+++ src/main/java/com/github/jeffrey/spring/boot/sfgmsscbrewerygateway/config/LocalHostRouteConfiguration.java
@@ -19,7 +19,10 @@ public class LocalHostRouteConfiguration {
                 .id("beer-service"))
                 .route(r -> r.path("/api/v1/customers*", "/api/v1/customers/**")
                 .uri("http://localhost:8081")
-                .id("beer-service"))
+                .id("beer-order-service"))
+                .route(r -> r.path("/api/v1/**")
+                .uri("http://localhost:8082")
+                .id("beer-inventory-service"))
                 .build();
     }
 }
